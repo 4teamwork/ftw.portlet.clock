257ab3a20a59f5c92d905e8dee5c689ecfd1db7a
Jonas Baumann <jone@jone.ch>
2012-11-12 14:37:37 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -10,28 +10,33 @@ tests_require = ['plone.app.testing',]
 setup(name='ftw.portlet.clock',
       version=version,
       description="Shows the sbb clock in a Plone portlet",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+
+      keywords='ftw portlet clock',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      url='http://plone.org/products/ftw.portlet.clock/',
+      url='https://github.com/4teamwork/ftw.portlet.clock',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', 'ftw.portlet', ],
       include_package_data=True,
       zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      install_requires=[
-          'setuptools',
-      ],
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
